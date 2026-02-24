2145d1082db42a5c66408bc9d16e388ce6916423
Shubham Londhe <shubhamlondhe976@gmail.com>
2026-02-24 20:32:03 +0530

    Update DuplicateFileRemoval.py

--- DuplicateFileRemoval.py
+++ DuplicateFileRemoval.py
@@ -1,3 +1,20 @@
+"""
+====================================================================
+Duplicate File Cleaner with Email Automation
+--------------------------------------------------------------------
+Author  : Shubham Londhe
+Purpose : 
+    - Scan directory recursively
+    - Identify duplicate files using MD5 checksum
+    - Delete duplicate copies (keep one original)
+    - Generate timestamped log file
+    - Send deletion report via email
+    - Execute automatically at scheduled intervals
+
+Usage :
+    python AddLogFileSendMail.py <DirectoryPath> <TimeInterval(min)> <ReceiverEmail>
+====================================================================
+"""
 
 import sys
 import time
@@ -7,152 +24,159 @@ import schedule
 import smtplib
 from email.message import EmailMessage
 
-def CalculateCheckSum(path, BlockSize = 1024):
-    fobj = open(path,"rb")
 
+# ---------------------------------------------------------------
+# Calculate MD5 checksum of file (used to detect duplicates)
+# ---------------------------------------------------------------
+def CalculateCheckSum(path, BlockSize=1024):
+    fobj = open(path, "rb")
     hobj = hashlib.md5()
 
+    # Read file in chunks to support large files
     buffer = fobj.read(BlockSize)
-    while(len(buffer) > 0):
+    while len(buffer) > 0:
         hobj.update(buffer)
         buffer = fobj.read(BlockSize)
 
     fobj.close()
-
     return hobj.hexdigest()
 
 
+# ---------------------------------------------------------------
+# Traverse directory and group files by checksum
+# ---------------------------------------------------------------
 def FindDuplicate(DirectoryName):
-    flag = os.path.isabs(DirectoryName)
 
-    if(flag == False):
+    # Convert relative path to absolute
+    if not os.path.isabs(DirectoryName):
         DirectoryName = os.path.abspath(DirectoryName)
 
-    flag = os.path.exists(DirectoryName)
-
-    if(flag == False):
-        print("The path is invalid")
+    # Validate path
+    if not os.path.exists(DirectoryName):
+        print("Invalid path")
         exit()
 
-    flag = os.path.isdir(DirectoryName)
-
-    if(flag == False):
-        print("Path is valid but target is not directory")
+    if not os.path.isdir(DirectoryName):
+        print("Path is not a directory")
         exit()
-    
+
     Duplicate = {}
+    TotalCount = 0
 
-    for FolderName, SubFolderNames, FileNames in os.walk(DirectoryName): 
-        TotalCount = 0                                                               
+    # Walk through directory recursively
+    for FolderName, SubFolderNames, FileNames in os.walk(DirectoryName):
         for fname in FileNames:
-            TotalCount = TotalCount + 1
-            fname = os.path.join(FolderName,fname)
+            TotalCount += 1
+            fname = os.path.join(FolderName, fname)
+
+            # Generate checksum
             checkSum = CalculateCheckSum(fname)
-            
+
+            # Group files by checksum
             if checkSum in Duplicate:
                 Duplicate[checkSum].append(fname)
             else:
-                Duplicate[checkSum] = [fname] 
-    
+                Duplicate[checkSum] = [fname]
+
     return Duplicate, TotalCount
 
+
+# ---------------------------------------------------------------
+# Create timestamped log file
+# ---------------------------------------------------------------
 def DirectoryLog():
-    if(os.path.exists("Marvellous")):
-        timestamp = time.ctime()
 
-        FileName = "MarvellousLog_%s.log" %(timestamp)
-        FileName = FileName.replace(" ","_")
-        FileName = FileName.replace(":","_")
-        FileName = os.path.join("Marvellous",FileName)
-        
-        fobj = open(FileName,"w")
+    if not os.path.exists("Logs"):
+        os.mkdir("Logs")
 
-        Border = "-"*80
+    timestamp = time.ctime()
+    FileName = "Log_%s.log" % (timestamp)
+    FileName = FileName.replace(" ", "_").replace(":", "_")
+    FileName = os.path.join("Logs", FileName)
 
-        fobj.write(Border+"\n")
-        fobj.write("This is a log file of Marvellous Automation Script \n")
-        fobj.write("This is a directory cleaner script used to delete duplicate files \n")
-        fobj.write(Border+"\n")
-        fobj.write("\n\n\n\n")
+    fobj = open(FileName, "w")
 
-        fobj.close()
-        return FileName
+    Border = "-" * 80
+    fobj.write(Border + "\n")
+    fobj.write("Duplicate File Cleaner Automation Log\n")
+    fobj.write(Border + "\n\n")
 
-    else:
-        os.mkdir("Marvellous")
-        timestamp = time.ctime()
+    fobj.close()
+    return FileName
 
-        FileName = "MarvellousLog_%s.log" %(timestamp)
-        FileName = FileName.replace(" ","_")
-        FileName = FileName.replace(":","_")
-        FileName = os.path.join("Marvellous",FileName)
-        
-        fobj = open(FileName,"w")
 
-        Border = "-"*80
-
-        fobj.write(Border+"\n")
-        fobj.write("This is a log file of Marvellous Automation Script \n")
-        fobj.write("This is a directory cleaner script used to delete duplicate files \n")
-        fobj.write(Border+"\n")
-        fobj.write("\n\n\n\n")
-
-        fobj.close()
-        return FileName
-
+# ---------------------------------------------------------------
+# Delete duplicates and send email report
+# ---------------------------------------------------------------
 def DeleteDuplicateSendMail(Path, Email):
+
     MyDict, TotalFiles = FindDuplicate(Path)
-    Result = list(filter(lambda X : len(X) > 1, MyDict.values()))
-    
+
+    # Filter only duplicates (checksum with more than 1 file)
+    Result = list(filter(lambda X: len(X) > 1, MyDict.values()))
+
     LogFile = DirectoryLog()
-    fobj = open(LogFile,"a")
-    Border = "-"*80
+    fobj = open(LogFile, "a")
+
+    Border = "-" * 80
     timestamp = time.ctime()
 
     Count = 0
-    Cnt = 0
-    Number = 0
-    fobj.write("Scanning started at : "+timestamp+"\n")
-    fobj.write("Names of deleted files are : \n")
+    DeletedCount = 0
+
+    fobj.write("Scanning started at : " + timestamp + "\n")
+    fobj.write("Deleted files list:\n")
+
+    # Keep first file, delete remaining
     for value in Result:
         for subValue in value:
             Count += 1
-            if(Count > 1):
-                fobj.write(subValue)
-                fobj.write("\n")
+            if Count > 1:
+                fobj.write(subValue + "\n")
                 os.remove(subValue)
-                Cnt += 1
+                DeletedCount += 1
         Count = 0
-    fobj.write("Total files scanned : "+str(TotalFiles)+"\n")
-    fobj.write("Total deleted file : ")
-    fobj.write(str(Cnt))
-    fobj.write("\n\n\n\n")
-    fobj.write(Border+"\n")
-    fobj.write("This file is created at : "+timestamp +"\n")
-    fobj.write(Border+"\n")
+
+    # Log summary
+    fobj.write("Total files scanned : " + str(TotalFiles) + "\n")
+    fobj.write("Total deleted files : " + str(DeletedCount) + "\n\n")
+    fobj.write(Border + "\n")
+    fobj.write("Log created at : " + timestamp + "\n")
+    fobj.write(Border + "\n")
+
     fobj.close()
-    print("Total deleted file :",Cnt)
 
-    SendMail(Email,LogFile)
+    print("Total deleted files:", DeletedCount)
+
+    # Send email with log attachment
+    SendMail(Email, LogFile)
 
 
-def SendMail(recaiver_mail, Attachment):
-    sender_mail = "roronoagreatestswordsman@gmail.com"
-    sender_password = "ijaz httg zodv edda"
+# ---------------------------------------------------------------
+# Send email with log attachment
+# ---------------------------------------------------------------
+def SendMail(receiver_mail, Attachment):
 
-    body = "These are name of deleted files"
-    # Create email
+    # ⚠️ Replace with environment variables in production
+    sender_mail = "your_email@gmail.com"
+    sender_password = "your_app_password"
+
+    body = "Attached is the duplicate file deletion report."
+
     msg = EmailMessage()
-    msg['Subject'] = "Deleted Duplicate Files"
+    msg['Subject'] = "Duplicate File Cleaner Report"
     msg['From'] = sender_mail
-    msg['To'] = recaiver_mail
+    msg['To'] = receiver_mail
     msg.set_content(body)
 
-    # Attach file
+    # Attach log file
     fobj = open(Attachment, 'rb')
-    msg.add_attachment(fobj.read(), maintype='application', subtype='octet-stream', filename=fobj.name)
+    msg.add_attachment(fobj.read(),
+                       maintype='application',
+                       subtype='octet-stream',
+                       filename=fobj.name)
 
-    # Send email
+    # Send mail
     smtp = smtplib.SMTP_SSL("smtp.gmail.com", 465)
     smtp.login(sender_mail, sender_password)
     smtp.send_message(msg)
@@ -161,43 +185,43 @@ def SendMail(recaiver_mail, Attachment):
     print("Email sent successfully!")
 
 
-
-
+# ---------------------------------------------------------------
+# Entry point
+# ---------------------------------------------------------------
 def main():
-    Border = "-"*63
+
+    Border = "-" * 63
     print(Border)
-    print("------------------- Marvellous Automation ---------------------")
+    print("----------- Duplicate File Cleaner Automation -----------")
     print(Border)
 
-    if(len(sys.argv) == 2):
-        if((sys.argv[1] == "--h") or (sys.argv[1] == "--H")):
-            print("This application is used to perform directory cleaning by deleting duplicate files and send email")
-            print("This is the directory automation script")
-        
-        elif((sys.argv[1] == "--u") or (sys.argv[1] == "--U")):
-            print("Use the given script as")
-            print("python AddLogFileSendMail.py  DirectoryPath  TimeInterval(in minutes)  ReceiverEmail")
-            print("Please provide valid absolute path")
+    # Help flags
+    if len(sys.argv) == 2:
 
-    elif(len(sys.argv) == 4):
-        schedule.every(int(sys.argv[2])).minutes.do(DeleteDuplicateSendMail, sys.argv[1], sys.argv[3])
+        if sys.argv[1].lower() == "--h":
+            print("Deletes duplicate files and emails report.")
+
+        elif sys.argv[1].lower() == "--u":
+            print("Usage:")
+            print("python AddLogFileSendMail.py <DirectoryPath> <TimeInterval(min)> <ReceiverEmail>")
+
+    # Normal execution
+    elif len(sys.argv) == 4:
+
+        schedule.every(int(sys.argv[2])).minutes.do(
+            DeleteDuplicateSendMail, sys.argv[1], sys.argv[3])
 
         while True:
             schedule.run_pending()
             time.sleep(1)
 
-    
     else:
-        print("Invalid number of command line arguments")
-        print("Use the given flags as :")
-        print("--h : Used to display the help")
-        print("--u : Used to display the usage")
-
+        print("Invalid arguments. Use --h or --u")
 
     print(Border)
-    print("---------------- Thank you for using our script ---------------")
-    print("------------------- Marvellous Infosystems --------------------")
+    print("--------------- Script Execution Finished ---------------")
     print(Border)
+
 
 if __name__ == "__main__":
     main()
